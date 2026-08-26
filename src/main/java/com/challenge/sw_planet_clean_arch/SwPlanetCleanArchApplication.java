a8ede3115966aacf6574ce15511559a78ea0f91f
Mirella Irene Artischeff <milla.artischeff@gmail.com>
2026-08-25 22:14:40 -0300

    first commit

---
+++ src/main/java/com/challenge/sw_planet_clean_arch/SwPlanetCleanArchApplication.java
@@ -0,0 +1,13 @@
+package com.challenge.sw_planet_clean_arch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwPlanetCleanArchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwPlanetCleanArchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/challenge/sw_planet_clean_arch/SwPlanetCleanArchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challenge.sw_planet_clean_arch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwPlanetCleanArchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
